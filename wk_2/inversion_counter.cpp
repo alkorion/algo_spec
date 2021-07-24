3c8cca222cd0f70f0392272ea707bc17ab703d5e
Alessandro Lira <al.lira.co@gmail.com>
2021-07-24 15:35:05 -0700

    Add skeletons for source files

---
+++ wk_2/inversion_counter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "inversion_counter.h"
+
+// array load_array_from_path(string file_path)
+
+// int count_invs(array a, int n)
+// int count_split_invs(array a, int n)
+
+int main() {
+
+    return 0;
+}
---
+++ wk_2/inversion_counter.h
@@ -0,0 +1,9 @@
+#ifndef INVERSION_COUNTER_H
+#define INVERSION_COUNTER_H
+
+// array load_array_from_path(string file_path)
+
+// int count_invs(array a, int n)
+// int count_split_invs(array a, int n)
+
+#endif
