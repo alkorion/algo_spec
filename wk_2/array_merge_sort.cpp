0dd3a2f631846e896177039bea9cb69109cc1c59
Alessandro Lira <al.lira.co@gmail.com>
2021-10-08 16:59:55 -0700

    Make first working version of merge-sort using pointersgit add array_merge_sort.cpp

--- wk_2/array_merge_sort.cpp
+++ wk_2/array_merge_sort.cpp
@@ -1,65 +1,114 @@
 #include <iostream>
+#include <vector>
 #include <array>
 
 constexpr int array_size = 8;
 
 using array = std::array<int,array_size>;
+using vector = std::vector<int>;
 
 void print_array(int* start, int* end) {
     std::cout << "[ ";
     for(int* iter = start; iter != end; ++iter) {
-        std::cout << *iter << ", ";
+        std::cout << *iter << " ";
     }
     std::cout << "]\n";
 }
 
-void merge(array a, int* left_start, int* left_end, int* right_start, int* right_end) {
-    // temp return array
-    array return_array;
-    int a_size = static_cast<int>(a.size());
+void merge(array& a, int* left_start, int* left_end, int* right_start, int* right_end) {
+    // temp return vector
+    vector return_array;
+    int k_size = right_end - left_start;
+    return_array.resize(k_size);
 
-    for (int k = 0; k < a_size; k++) {
-        // end cases check
-        if (left_start == left_end) {
+    int* left_iter = left_start;
+    int* right_iter = right_start;
+
+    for (int k = 0; k < k_size; k++) {
+        // end cases check for when either array is empty
+        if (left_iter == left_end) {
             // append the rest of right to return_array, then break
-            while (right_start != right_end) {
-                return_array[k] = *right_start;
-                ++right_start;
+            while (right_iter != right_end) {
+                return_array[k] = *right_iter;
+                ++right_iter;
                 ++k;
             }
             break;
         }
-        else if (right_start == right_end) {
+        else if (right_iter == right_end) {
             // append the rest of left to return_array, then break
-            while (left_start != left_end) {
-                return_array[k] = *left_start;
-                ++left_start;
+            while (left_iter != left_end) {
+                return_array[k] = *left_iter;
+                ++left_iter;
                 ++k;
             }
             break;
         }
-        
-        if (*left_start < *right_start) {
-            return_array[k] = *left_start;
-            ++left_start;
+
+        // standard comparison between both arrays
+        if (*left_iter < *right_iter) {
+            return_array[k] = *left_iter;
+            ++left_iter;
         }
         else { // assuming no equal values in array
-            return_array[k] = *right_start;
-            ++right_start;
+            return_array[k] = *right_iter;
+            ++right_iter;
         }
     }
+    
+    std::cout << "Return array: "; print_array(&return_array[0], &return_array[k_size-1]);
 
-    print_array(return_array.begin(), return_array.end());
+    int* a_iter = left_start;
+    // copy over return array into original array
+    for(int i = 0; i < k_size; ++i) {
+        *a_iter = return_array[i];
+        ++a_iter;
+    }
 
 }
 
+void merge_sort(array& a, int* start, int* end) {
+    int n = end - start;
+
+    // base case, no re-arranging needed
+    if (n == 1) {
+        return; 
+    }
+
+    int* left_start = start;
+    int* left_end = left_start + n/2;
+
+    int* right_start = left_end;
+    int* right_end = end;
+
+    // sort each half of the original array in place
+    merge_sort(a, left_start, left_end);
+    merge_sort(a, right_start, right_end);
+
+    // merge together the two sub-arrays in place
+    merge(a, left_start, left_end, right_start, right_end);
+
+}
 
 int main() {
     
-    array a = { 1, 3, 5, 7, 2, 4, 6, 8 };
+    // array a = { 1, 3, 5, 7, 2, 4, 6, 8 };
+    array a = { 8, 7, 6, 5, 4, 3, 2, 1 };
+    // array a = { 1, 3, 2, 4 };
+    // array a = { 4, 3, 2 ,1 };
+
+
+
     print_array(a.begin(), a.end());
 
-    merge(a, a.begin(), a.begin()+4, a.begin()+4, a.end());
+    // merge(a, a.begin(), a.begin()+4, a.begin()+4, a.end());
+
+    // print_array(a.begin(), a.end());
+
+    merge_sort(a, a.begin(), a.end());
+
+    print_array(a.begin(), a.end());
+
 
     return 0;
 }
